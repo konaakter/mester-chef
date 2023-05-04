bc01f892d5396ebd57c960735c96f9abeaba79a1
unknown <kona>
2023-05-04 23:53:27 +0600

    dropdwon

--- src/Share/Header/Header.jsx
+++ src/Share/Header/Header.jsx
@@ -22,23 +22,40 @@ const Header = () => {
 
                 <div className="navbar  container mx-auto text-slate-50">
                     <div className="navbar-start">
-                        <div className="dropdown">
+                        <div className="dropdown bg-color ">
                             <label tabIndex={0} className="btn btn-ghost lg:hidden">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
-                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li tabIndex={0}>
-                                    <a className="justify-between">
-                                        Parent
-                                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                    </a>
-                                    <ul className="p-2">
-                                        <li><a>Submenu 1</a></li>
-                                        <li><a>Submenu 2</a></li>
+                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
+                            <li>
+                                <NavLink
+                                    to='/'
+                                    className={({ isActive }) => (isActive ? 'active' : 'default')}
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+
+
+                                    <ul className="p-2  ">
+                                        <li>
+                                            <NavLink
+                                                to='/logi'
+                                                className={({ isActive }) => (isActive ? 'active' : 'default')}
+                                            >
+                                                contract
+                                            </NavLink>
+                                        </li>
+
                                     </ul>
+                                <li>
+                                    <NavLink
+                                        to='/blog'
+                                        className={({ isActive }) => (isActive ? 'active' : 'default')}
+                                    >
+                                        blog
+                                    </NavLink>
                                 </li>
-                                <li><a>Item 3</a></li>
                             </ul>
                         </div>
                         <a className=" normal-case text-xl font-bold color">Chef-Point</a>
