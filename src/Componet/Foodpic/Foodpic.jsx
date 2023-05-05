39686f068810179a8457fa29eb89359255c7fb64
unknown <kona>
2023-05-05 18:43:44 +0600

    img gap set

--- src/Componet/Foodpic/Foodpic.jsx
+++ src/Componet/Foodpic/Foodpic.jsx
@@ -9,7 +9,7 @@ const Foodpic = () => {
         <div className=''>
             <div className='w-4/5 mx-auto pb-40'>
                 <h1 className='text-center text-6xl font-bold color pb-20'>ToP FOOd</h1>
-                <div className=' lg:grid grid-cols-3'>
+                <div className=' lg:grid grid-cols-3 gap-6'>
                     <div className='card w-96 bg-base-100 shadow-xl image-full ' >
                         <figure><img src={logo1} alt="Shoes" /></figure>
                         <div className="card-body">
@@ -34,10 +34,10 @@ const Foodpic = () => {
                             </div>
                         </div>
                     </div>
-                    </div>
                 </div>
             </div>
-            );
+        </div>
+    );
 };
 
-            export default Foodpic;
+export default Foodpic;
